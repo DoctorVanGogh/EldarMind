5f22cb0cf16b6da7a1562ef77268a78c2f3cd8e9
DoctorVanGogh <DoctorVanGogh@users.noreply.github.com>
2014-05-20 00:48:24 +0200

    updated suggestion form started auto suggest logic

--- EldarMind.lua
+++ EldarMind.lua
@@ -9,10 +9,12 @@ require "Window"
 -----------------------------------------------------------------------------------------------
 -- Constants
 -----------------------------------------------------------------------------------------------
-
-
 local MAJOR, MINOR = "EldarMind-1.0", 1
 local glog
+
+local kstrDefaultSprite = "IconSprites:Icon_ItemArmorChest_shirt_0001";
+local kstrPatternTooltipStringFormula = "<P Font=\"CRB_InterfaceLarge_B\" TextColor=\"ff9d9d9d\">%s</P><P Font=\"CRB_InterfaceSmall\" TextColor=\"ff9d9d9d\">%s</P>"
+
 
 -----------------------------------------------------------------------------------------------
 -- Initialization
@@ -33,6 +35,7 @@ function EldarMind:OnInitialize()
 		pattern = "%d [%c:%n] %l - %m",
 		appender = "GeminiConsole"
 	})	
+	self.log = glog
 	self.xmlDoc = XmlDoc.CreateFromFile("EldarMind.xml")
 	self.xmlDoc:RegisterCallback("OnDocumentReady", self) 	
 	
@@ -54,15 +57,86 @@ function EldarMind:OnDocumentReady()
 	end
 	
 	self.wndMain = Apollo.LoadForm(self.xmlDoc, "EldarMindForm", nil, self)
-	self.wndMain:FindChild("HeaderLabel"):SetText(MAJOR)
 	self.xmlDoc = nil;
 	
 	Apollo.RegisterSlashCommand("em", "OnSlashCommand", self)		
 	self.wndMain:Show(false);
+	
+	end
+	Apollo.RegisterEventHandler("InvokeScientistExperimentation", "OnInvokeScientistExperimentation", self)
+	Apollo.RegisterEventHandler("ScientistExperimentationResult", "OnScientistExperimentationResult", self)	
 end
+
+function EldarMind:InitializeForm()
+	self.wndMain:FindChild("HeaderLabel"):SetText(MAJOR)	
+
+	if self.locSavedWindowLoc then
+		self.wndMain:MoveToLocation(self.locSavedWindowLoc)
+	end		
+end
+
+function EldarMind:UpdateSuggestion(pmExperiment, arResults)
+
+	if pmExperiment ~= nil then
+		self.guesses = {}
+		self.pmExperiment = pmExperiment
+		
+		-- local tInfo = pmExperiment:GetScientistExperimentationInfo()
+		-- tInfo.nAttempts
+		
+		-- local tPatterns = pmExperiment:GetScientistExperimentationCurrentPatterns()
+		-- for idx, tPattern in pairs(tPatterns) do
+		--    tPattern.strIcon -- SPRITE
+		--    tPattern.strName, tPattern.strDescription
+		
+	end
+
+
+	if arResults ~= nil then
+		
+	
+	end
+	
+	
+	local tSuggestBtns = {self.wndMain:FindChild("Suggestion1"), self.wndMain:FindChild("Suggestion2"), self.wndMain:FindChild("Suggestion3"), self.wndMain:FindChild("Suggestion4")}
+
+	--local tPatterns = pmExperiment:GetScientistExperimentationCurrentPatterns()
+	--if tPatterns == nil then
+	--	for key, wndSuggestBtn in pairs(tSuggestBtns ) do
+	--		local icon = wndSuggestBtn:FindChild("SuggestionIcon")
+	--		icon:SetTooltip("")		
+	--		icon:SetSprite(kstrDefaultSprite)
+	--	end
+	--else
+	--	
+	--end
+	
+end
+
 -----------------------------------------------------------------------------------------------
 -- EldarMind logic
 -----------------------------------------------------------------------------------------------
+function EldarMind:OnInvokeScientistExperimentation(pmExperiment)
+	self:InitializeForm()
+			
+	self:UpdateSuggestion(pmExperiment, nil)	
+	
+	if not self.wndMain:IsVisible() then
+		self:ToggleWindow()	
+	end
+end
+
+
+function EldarMind:OnScientistExperimentationResult(arResults)		
+	self:UpdateSuggestion(nil, arResults)
+		
+	if not self.wndMain:IsVisible() then
+		self:ToggleWindow()	
+	end
+end
+
+
+
 function EldarMind:OnSlashCommand()
 	if not self.wndMain then
 		return
@@ -75,41 +149,39 @@ end
 -- Persistence
 -----------------------------------------------------------------------------------------------
 function EldarMind:OnSaveSettings(eLevel)
-	-- We save at character level,
-	if (eLevel ~= GameLib.CodeEnumAddonSaveLevel.Character) then
-		return
-	end
-
 	glog:debug("OnSaveSettings")	
-		
-	local tSave = { 
-		version = {
-			MAJOR = MAJOR,
-			MINOR = MINOR
-		}, 
-	}
 	
-	return tSave
+    if eLevel == GameLib.CodeEnumAddonSaveLevel.Account then				
+		local locWindowLocation = self.wndMain and self.wndMain:GetLocation() or self.locSavedWindowLoc				
+	
+		local tSave = { 
+			version = {
+				MAJOR = MAJOR,
+				MINOR = MINOR
+			}, 		
+			tWindowLocation = locWindowLocation and locWindowLocation:ToTable() or nil
+		}	
+	end	
 end
 
 
 function EldarMind:OnRestoreSettings(eLevel, tSavedData)
-	-- We restore at character level,
-	if (eLevel ~= GameLib.CodeEnumAddonSaveLevel.Character) then
-		return
-	end
-	
 	glog:debug("OnRestoreSettings")
-	
+
 	if not tSavedData or tSavedData.version.MAJOR ~= MAJOR then
 		return
 	end	
 	
+    if eLevel == GameLib.CodeEnumAddonSaveLevel.Account then						
+		if tSavedData.tWindowLocation then
+			self.locSavedWindowLoc = WindowLocation.new(tSavedData.tWindowLocation)
+		end	
 	
+	end		
 end
 
 -----------------------------------------------------------------------------------------------
--- EldarMind Functions
+-- EldarMindForm Functions
 -----------------------------------------------------------------------------------------------
 function EldarMind:ToggleWindow()
 	if self.wndMain:IsVisible() then
@@ -118,4 +190,8 @@ function EldarMind:ToggleWindow()
 		self.wndMain:Show(true)
 		self.wndMain:ToFront()
 	end
+end
+
+function EldarMind:WindowMove()
+	self.locSavedWindowLoc = self.wndMain:GetLocation()
 end
