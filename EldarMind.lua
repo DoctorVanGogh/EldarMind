59e28bd7741dc3fd1e3ad425f09b53ea870c86de
DoctorVanGogh <DoctorVanGogh@users.noreply.github.com>
2014-05-21 04:34:05 +0200

    added experiment suggestion & tracking code

--- EldarMind.lua
+++ EldarMind.lua
@@ -12,9 +12,10 @@ require "Window"
 local MAJOR, MINOR = "EldarMind-1.0", 1
 local glog
 
-local kstrDefaultSprite = "IconSprites:Icon_ItemArmorChest_shirt_0001";
+local kstrDefaultSprite = "IconSprites:Icon_ItemMisc_AcceleratedOmniplasm";
 local kstrPatternTooltipStringFormula = "<P Font=\"CRB_InterfaceLarge_B\" TextColor=\"ff9d9d9d\">%s</P><P Font=\"CRB_InterfaceSmall\" TextColor=\"ff9d9d9d\">%s</P>"
 
+local kstrAttemptExperimentationFunction = "AttemptScientistExperimentation"
 
 -----------------------------------------------------------------------------------------------
 -- Initialization
@@ -26,8 +27,15 @@ local EldarMind = Apollo.GetPackage("Gemini:Addon-1.0").tPackage:NewAddon(
 																	{ 
 																		"Gemini:Logging-1.2",
 																		"DoctorVanGogh:Lib:Mastermind:P4C4R:Knuth-1.0"
-																	})
+																	},
+																	"Gemini:Hook-1.0")
 
+EldarMind.States = {
+	Waiting = 1,
+	Running = 2,
+	Mismatch = 3
+}																	
+																
 function EldarMind:OnInitialize()
 	local GeminiLogging = Apollo.GetPackage("Gemini:Logging-1.2").tPackage
 	glog = GeminiLogging:GetLogger({
@@ -40,6 +48,8 @@ function EldarMind:OnInitialize()
 	self.xmlDoc:RegisterCallback("OnDocumentReady", self) 	
 	
 	self.lookup = Apollo.GetPackage("DoctorVanGogh:Lib:Mastermind:P4C4R:Knuth-1.0").tPackage;	
+		
+	self:SetState(EldarMind.States.Waiting)
 end
 
 
@@ -68,49 +78,85 @@ function EldarMind:OnDocumentReady()
 end
 
 function EldarMind:InitializeForm()
+	if not self.wndMain then
+		return
+	end
+
 	self.wndMain:FindChild("HeaderLabel"):SetText(MAJOR)	
 
 	if self.locSavedWindowLoc then
 		self.wndMain:MoveToLocation(self.locSavedWindowLoc)
-	end		
+	end	
+	
+	self:UpdateForm()	
 end
 
-function EldarMind:UpdateSuggestion(pmExperiment, arResults)
+function EldarMind:UpdateForm(pmExperiment, arResults)
+
+	if not self.wndMain then
+		return
+	end	
+	
+	self.wndMain:FindChild("BlockerMismatch"):SetVisible(self:GetState() == EldarMind.States.Mismatch)
+	self.wndMain:FindChild("BlockerWaiting"):SetVisible(self:GetState() == EldarMind.States.Waiting)	
 
 	if pmExperiment ~= nil then
 		self.guesses = {}
 		self.pmExperiment = pmExperiment
+		self.tPatterns = pmExperiment:GetScientistExperimentationCurrentPatterns()		
 		
-		-- local tInfo = pmExperiment:GetScientistExperimentationInfo()
-		-- tInfo.nAttempts
-		
-		-- local tPatterns = pmExperiment:GetScientistExperimentationCurrentPatterns()
-		-- for idx, tPattern in pairs(tPatterns) do
-		--    tPattern.strIcon -- SPRITE
-		--    tPattern.strName, tPattern.strDescription
-		
+		if not self:IsHooked(pmExperiment, kstrAttemptExperimentationFunction) then
+			self:Hook(pmExperiment, kstrAttemptExperimentationFunction, "PreAttemptExperimentation")
+		end						
 	end
 
-
-	if arResults ~= nil then
-		
+	if arResults ~= nil then	
+		local nExact = 0
+		local nPartial = 0
+		for idx, eCurrResult in ipairs(arResults) do
+			if eCurrResult == PathMission.ScientistExperimentationResult_Correct then
+				nExact = nExact + 1
+			elseif eCurrResult == PathMission.ScientistExperimentationResult_CorrectPattern then
+				nPartial = nPartial + 1
+			end
+		end	
 	
+		self.guesses[#self.guesses].result = tuple(nExact, nPartial)		
+	end	
+	
+	self:UpdateSuggestions()	
+end
+
+function EldarMind:UpdateSuggestions()
+	
+	local suggestion = self:EldarMind:GetCurrentSuggestion()
+	local p1, p2, p3, p4
+	
+	if suggestion then
+		p1, p2, p3, p4 = suggestion()
+	else
+		p1, p2, p3, p4 = false, false, false, false
 	end
+		
+	local tSuggestBtns = {
+		[self.wndMain:FindChild("Suggestion1")] = p1, 
+		[self.wndMain:FindChild("Suggestion2")] = p2, 
+		[self.wndMain:FindChild("Suggestion3")] = p3, 
+		[self.wndMain:FindChild("Suggestion4")] = p4
+	}
 	
-	
-	local tSuggestBtns = {self.wndMain:FindChild("Suggestion1"), self.wndMain:FindChild("Suggestion2"), self.wndMain:FindChild("Suggestion3"), self.wndMain:FindChild("Suggestion4")}
+	for wndSuggestion, p in pairs(tSuggestBtns) do
+		local icon = wndSuggestBtn:FindChild("SuggestionIcon")
+		if p then
+			local pattern = self.tPatterns[p]
+			icon:SetTooltip(string.format(kstrPatternTooltipStringFormula, pattern.strName, pattern.strDescription))		
+			icon:SetSprite(pattern.strIcon)					
+		else
+			icon:SetTooltip("")		
+			icon:SetSprite(kstrDefaultSprite)			
+		end
+	end
 
-	--local tPatterns = pmExperiment:GetScientistExperimentationCurrentPatterns()
-	--if tPatterns == nil then
-	--	for key, wndSuggestBtn in pairs(tSuggestBtns ) do
-	--		local icon = wndSuggestBtn:FindChild("SuggestionIcon")
-	--		icon:SetTooltip("")		
-	--		icon:SetSprite(kstrDefaultSprite)
-	--	end
-	--else
-	--	
-	--end
-	
 end
 
 -----------------------------------------------------------------------------------------------
@@ -119,7 +165,7 @@ end
 function EldarMind:OnInvokeScientistExperimentation(pmExperiment)
 	self:InitializeForm()
 			
-	self:UpdateSuggestion(pmExperiment, nil)	
+	self:UpdateForm(pmExperiment, nil)	
 	
 	if not self.wndMain:IsVisible() then
 		self:ToggleWindow()	
@@ -128,13 +174,43 @@ end
 
 
 function EldarMind:OnScientistExperimentationResult(arResults)		
-	self:UpdateSuggestion(nil, arResults)
+	self:UpdateForm(nil, arResults)
 		
 	if not self.wndMain:IsVisible() then
 		self:ToggleWindow()	
 	end
 end
 
+function EldarMind:GetCurrentSuggestion()
+	if not self.tPatterns or not self.guesses then
+		return
+	end
+	
+	local suggestion = self.lookup
+	for idx, guess in ipairs(self.guesses) do
+		if guess.result then
+			suggestion = suggestion[guess.result]
+		end
+	end
+	
+	if not suggestion then
+		return
+	end
+	
+	return suggestion.guess	
+end
+
+function EldarMind:PreAttemptExperimentation(numPatterns, tCode)
+	local suggestion = self:EldarMind:GetCurrentSuggestion()
+	local p1, p2, p3, p4 = suggestion()
+	
+	if tCode.Choice1 ~= p1 or tCode.Choice2 ~= p2 or  tCode.Choice3 ~= p3 or tCode.Choice4 ~= p4 then
+		self:SetState(EldarMind.States.Mismatch)
+		return		
+	end
+	
+	table.insert(self.guesses, {guess = suggestion})	
+end
 
 
 function EldarMind:OnSlashCommand()
@@ -143,6 +219,23 @@ function EldarMind:OnSlashCommand()
 	end
 	
 	self:ToggleWindow()
+end
+
+function EldarMind:GetState()
+	return self.state
+end
+
+function EldarMind:SetState(state)
+	if state == self.state then
+		return
+	end
+
+	if not EldarMind.States[state]  then
+		return
+	end
+	
+	self.state = state
+	self:UpdateForm()
 end
 
 -----------------------------------------------------------------------------------------------
